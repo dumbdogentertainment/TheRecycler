9f5fd7822579902c49e3a5e723d797251dccdcca
Robert <packleader@dumbdogentertainment.com>
2018-08-11 20:45:37 -0500

    -- missing files from version control .. updated web player settings;

---
+++ TheRecycler/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance = null;
+
+    public Image PowerBar;
+
+    void Awake()
+    {
+        Instance = this;
+        this.PowerBar.fillAmount = 1f;
+    }
+
+    void OnApplicationQuit()
+    {
+        Instance = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DestroyAllTrashBags();
+            this.PowerBar.fillAmount = 1f;
+        }
+
+        this.PowerBar.fillAmount -= Time.deltaTime * 0.05f;
+    }
+
+    public void DestroyAllTrashBags()
+    {
+        GameObject.FindGameObjectsWithTag("TrashBags")
+            .ToList()
+            .ForEach(Destroy);
+    }
+}
---
+++ TheRecycler/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] TrashBagPrefabs;
+    public GameObject GameBoard;
+
+    private BoxCollider2D spawnerCollider;
+
+    private float spawnCooldownRemaining;
+
+    void Start()
+    {
+        this.spawnerCollider = GetComponent<BoxCollider2D>();
+        this.spawnCooldownRemaining = Random.Range(0.15f, 0.75f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.spawnCooldownRemaining -= Time.deltaTime;
+
+        if (this.spawnCooldownRemaining <= 0f)
+        {
+            SpawnTrashBag();
+            this.spawnCooldownRemaining = Random.Range(0.15f, 0.75f);
+        }
+    }
+
+    void SpawnTrashBag()
+    {
+        if (this.spawnerCollider.IsTouchingLayers())
+        {
+            return;
+        }
+
+        var randomIndex = Random.Range(0, this.TrashBagPrefabs.Length - 1);
+        Instantiate(this.TrashBagPrefabs[randomIndex], this.transform.position, Quaternion.identity, this.GameBoard.transform);
+    }
+}
